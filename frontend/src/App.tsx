2d7476e24bda7b14fd94abb74fbcaebc82d88d3f
Vasu Verma <vermavasu069@gmail.com>
2024-09-22 23:40:43 +0530

    File upload page for admin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Input from "./pages/Input"
 import Admin from "./pages/Admin"
+import Upload from "./pages/Upload"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Input />} />
         <Route path="/admin" element={<Admin />} />
+        <Route path="/upload" element={<Upload />} />
       </Routes>
     </BrowserRouter>
   )
